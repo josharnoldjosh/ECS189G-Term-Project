bae2159c0917b42b1fbf72bbf3a280e4c558fd70
JOSH ARNOLD <jarnold@ucdavis.edu>
2020-03-04 21:39:37 -0800

    update data_summary.R

--- data_summary.R
+++ data_summary.R
@@ -1,6 +1,16 @@
 # Author: Josh Arnold
 # Data Summary
 
+# InstEval Data summary
 library(lme4)
 data <- InstEval
 summary(data)
+
+# Song Dataset Summary
+songs <- read.csv(file = 'songsDataset.csv')
+summary(songs)
+
+# Histogram
+library(ggplot2)
+ggplot(InstEval, aes(x=y)) + geom_histogram(binwidth=0.5) + theme_light()
+ggplot(songs, aes(x=X.rating.)) + geom_histogram(binwidth=0.5) + theme_light()
