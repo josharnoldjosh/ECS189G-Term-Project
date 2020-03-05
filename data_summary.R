ab4b0ee1632e2ce78565cbe2a9e10595e4967f8b
JOSH ARNOLD <jarnold@ucdavis.edu>
2020-03-04 22:23:52 -0800

    rename nmf

--- data_summary.R
+++ data_summary.R
@@ -10,7 +10,12 @@ summary(data)
 songs <- read.csv(file = 'songsDataset.csv')
 summary(songs)
 
+?InstEval
+
 # Histogram
 library(ggplot2)
 ggplot(InstEval, aes(x=y)) + geom_histogram(binwidth=0.5) + theme_light()
 ggplot(songs, aes(x=X.rating.)) + geom_histogram(binwidth=0.5) + theme_light()
+
+head(songs)
+head(InstEval)
