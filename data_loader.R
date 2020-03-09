8d3d8e2068d68eb07ac044365917f83ab9df6dcb
JOSH ARNOLD <jarnold@ucdavis.edu>
2020-03-08 23:37:28 -0700

    updated data loader and KNN

--- data_loader.R
+++ data_loader.R
@@ -1,14 +1,56 @@
-# This function loads data into memory
-load_data <- function(y_is_last_column=FALSE) {
-  library(lme4)
-  names(InstEval) <- c("user_id", "item_id", "studage", "lectage", "service", "dept", "y")
-  if (!y_is_last_column) {
-    InstEval <- InstEval[, c("user_id", "item_id", "y", "studage", "lectage", "service", "dept")]
+embedMeans <- function(data) {
+  library(rectools)
+  if (!all(names(data)[1:3] == c("UserID", "ItemID", "rating"))) {
+    stop("Make sure y/ratings isn't last column when embedding means.")
   }
+  
+  # Get user data
+  user_data <- formUserData(data)
+  
+  # Update matrix
+  f <- function(x) {
+    return (mean(user_data[[as.integer(x)]]$ratings))
+  }
+  
+  data$ItemID <- NULL
+  data$UserID <- sapply(data$UserID, f)
+  return (data)
+}
+
+# This function loads data into memory
+load_data <- function(is_y_last=F) {
+  library(lme4)
+  library(regtools)
+  names(InstEval) <- c("UserID", "ItemID", "studage", "lectage", "service", "dept", "rating")
+  if (!is_y_last) {
+    InstEval <- InstEval[, c("UserID", "ItemID", "rating", "studage", "lectage", "service", "dept")]
+  }
+  # Convert factors to dummies... ?
   SongList <- read.csv(file = 'songsDataset.csv')
-  names(SongList) <- c("user_id", "item_id", "y")
+  names(SongList) <- c("UserID", "ItemID", "rating")
   datasets <- list()
   datasets$InstEval <- InstEval
   datasets$SongList <- SongList
   return (datasets)
+}
+
+# Train test split
+train_test_split <- function(data, test_ratio=0.3) {
+  library(caret)
+  train.index <- createDataPartition(data$y, p = (1-test_ratio), list = FALSE)
+  train <- data[ train.index,]
+  test  <- data[-train.index,]
+  result <- list()
+  result$train <- train
+  result$test <- test
+  return (result)
+}
+
+# This function only needs to be run once, but probably never
+# Check if output files exist
+init <- function() {
+  library(rectools)
+  datasets <- load_data(is_y_last = FALSE)
+  formUserData(datasets$SongList, fileOut = "SongListUserData")
+  formUserData(datasets$InstEval, fileOut = "InstEvalUserData")
 }
--- knn.R
+++ knn.R
@@ -1,14 +1,15 @@
 # KNN Function. Note, both x and y must have the EXACT SAME columns.
 # Just the number of rows changes.
 # Thus, we could split the data simply by rows and not even consider the rating or y value.
-custom_knn <- function(x, y, test, nc) {
+custom_knn <- function(df, test, nc) {
   library(regtools)
-  result <- kNN(as.matrix(x), as.matrix(y), as.matrix(test), nc)
+  x <- df
+  y <- df$y
+  result <- kNN(x, y, test, nc)
   return (result)
 }
 
 # Load data
 source('./data_loader.R')
-datasets <- load_data(y_is_last_column = FALSE)
-
-head(inst_eval_data)
+datasets <- load_data(is_y_last = FALSE)
+datasets$SongList <- embedMeans(datasets$SongList)
